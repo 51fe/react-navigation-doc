738f0210badfe57202a776ce6e596f16f8593c18
51fe <riafan@hotmail.com>
2023-08-12 20:52:17 +0800

    svg jsx fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ module.exports = {
   projectName: 'react-navigation.github.io',
   scripts: [
     'https://buttons.github.io/buttons.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+    'https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js',
     '/js/code-block-buttons.js',
   ],
   themeConfig: {
